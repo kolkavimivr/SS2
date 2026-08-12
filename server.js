2416ce1c26f9a5be59f3554c691e4f7fa1ed8dab
kolkavimivr <kolkavimivr@gmail.com>
2026-08-13 01:19:58 +0300

    89

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.all(['/survey', '/ivr', '/'], (req, res) => {
-    // הגדרת Content-Type כטקסט נקי
+    // הגדרת Content-Type כטקסט נקי עבור ימות המשיח
     res.setHeader('Content-Type', 'text/plain; charset=utf-8');
 
-    // איחוד הפרמטרים הנכנסים מימות המשיח (GET / POST)
+    // איחוד פרמטרים מ-GET ו-POST
     const params = { ...req.query, ...req.body };
 
     // 1. טיפול בניתוק שיחה
@@ -17,37 +17,37 @@ app.all(['/survey', '/ivr', '/'], (req, res) => {
     }
 
     // -------------------------------------------------------------
-    // 2. בדיקת שאלות ברצף (כולל בקשת אישור "לאישור הקישו 1")
+    // 2. בדיקת שאלות ברצף (תיקון תחביר: read=VARIABLE=FILE)
     // -------------------------------------------------------------
 
     // שאלה 01: פתיח ראשי + שאלה q01
     if (!params.q01) {
-        return res.send('id_list_message=f-quiz_intro&read=f-q01=q01,yes,1,1,7,Number,yes,no,1.2.3');
+        return res.send('id_list_message=f-quiz_intro&read=q01=f-q01,yes,1,1,7,Number,yes,no,1.2.3');
     }
 
     // שאלות 02 עד 14
-    if (!params.q02) return res.send('read=f-q02=q02,yes,1,1,7,Number,yes,no,1.2.3');
-    if (!params.q03) return res.send('read=f-q03=q03,yes,1,1,7,Number,yes,no,1.2.3');
-    if (!params.q04) return res.send('read=f-q04=q04,yes,1,1,7,Number,yes,no,1.2.3');
-    if (!params.q05) return res.send('read=f-q05=q05,yes,1,1,7,Number,yes,no,1.2.3');
-    if (!params.q06) return res.send('read=f-q06=q06,yes,1,1,7,Number,yes,no,1.2.3');
-    if (!params.q07) return res.send('read=f-q07=q07,yes,1,1,7,Number,yes,no,1.2.3');
-    if (!params.q08) return res.send('read=f-q08=q08,yes,1,1,7,Number,yes,no,1.2.3');
-    if (!params.q09) return res.send('read=f-q09=q09,yes,1,1,7,Number,yes,no,1.2.3');
-    if (!params.q10) return res.send('read=f-q10=q10,yes,1,1,7,Number,yes,no,1.2.3');
-    if (!params.q11) return res.send('read=f-q11=q11,yes,1,1,7,Number,yes,no,1.2.3');
-    if (!params.q12) return res.send('read=f-q12=q12,yes,1,1,7,Number,yes,no,1.2.3');
-    if (!params.q13) return res.send('read=f-q13=q13,yes,1,1,7,Number,yes,no,1.2.3');
-    if (!params.q14) return res.send('read=f-q14=q14,yes,1,1,7,Number,yes,no,1.2.3');
+    if (!params.q02) return res.send('read=q02=f-q02,yes,1,1,7,Number,yes,no,1.2.3');
+    if (!params.q03) return res.send('read=q03=f-q03,yes,1,1,7,Number,yes,no,1.2.3');
+    if (!params.q04) return res.send('read=q04=f-q04,yes,1,1,7,Number,yes,no,1.2.3');
+    if (!params.q05) return res.send('read=q05=f-q05,yes,1,1,7,Number,yes,no,1.2.3');
+    if (!params.q06) return res.send('read=q06=f-q06,yes,1,1,7,Number,yes,no,1.2.3');
+    if (!params.q07) return res.send('read=q07=f-q07,yes,1,1,7,Number,yes,no,1.2.3');
+    if (!params.q08) return res.send('read=q08=f-q08,yes,1,1,7,Number,yes,no,1.2.3');
+    if (!params.q09) return res.send('read=q09=f-q09,yes,1,1,7,Number,yes,no,1.2.3');
+    if (!params.q10) return res.send('read=q10=f-q10,yes,1,1,7,Number,yes,no,1.2.3');
+    if (!params.q11) return res.send('read=q11=f-q11,yes,1,1,7,Number,yes,no,1.2.3');
+    if (!params.q12) return res.send('read=q12=f-q12,yes,1,1,7,Number,yes,no,1.2.3');
+    if (!params.q13) return res.send('read=q13=f-q13,yes,1,1,7,Number,yes,no,1.2.3');
+    if (!params.q14) return res.send('read=q14=f-q14,yes,1,1,7,Number,yes,no,1.2.3');
 
     // שאלה 15: מעברון חלק ג' + שאלה q15
     if (!params.q15) {
-        return res.send('id_list_message=f-mid_intro3&read=f-q15=q15,yes,1,1,7,Number,yes,no,1.2.3');
+        return res.send('id_list_message=f-mid_intro3&read=q15=f-q15,yes,1,1,7,Number,yes,no,1.2.3');
     }
 
     // שאלות 16 ו-17
-    if (!params.q16) return res.send('read=f-q16=q16,yes,1,1,7,Number,yes,no,1.2.3');
-    if (!params.q17) return res.send('read=f-q17=q17,yes,1,1,7,Number,yes,no,1.2.3');
+    if (!params.q16) return res.send('read=q16=f-q16,yes,1,1,7,Number,yes,no,1.2.3');
+    if (!params.q17) return res.send('read=q17=f-q17,yes,1,1,7,Number,yes,no,1.2.3');
 
     // -------------------------------------------------------------
     // 3. שלב סיום: חישוב תוצאות ומשוב
@@ -67,6 +67,7 @@ app.all(['/survey', '/ivr', '/'], (req, res) => {
         m_hr: 0
     };
 
+    // חישוב ניקוד לפי הבחירות בזוגות
     const pairwiseQuestions = [
         { ans: q01, a: 'm_accounting', b: 'm_graphics' },
         { ans: q02, a: 'm_tech',       b: 'm_education' },
@@ -90,8 +91,10 @@ app.all(['/survey', '/ivr', '/'], (req, res) => {
         }
     });
 
+    // מיון המגמות מהציון הגבוה לנמוך
     const sortedMajors = Object.keys(scores).sort((a, b) => scores[b] - scores[a]);
 
+    // בניית רשימת השמעה לתוצאות
     const playList = [
         'quiz_results',
         sortedMajors[0],
@@ -99,6 +102,7 @@ app.all(['/survey', '/ivr', '/'], (req, res) => {
         sortedMajors[2]
     ];
 
+    // תנאי משוב מיוחדים לפי שאלות 15-17
     if (q15 === '1' || q17 === '1') playList.push('warning_social');
     if (q16 === '1') playList.push('info_minimal_load');
     if (q15 === '2' && q17 === '2') playList.push('info_independent');
